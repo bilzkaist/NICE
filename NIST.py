a054070a46c96395e092cccbb88347a08b71c460
bilzkaist <63588157+bilzkaist@users.noreply.github.com>
2022-09-10 23:51:32 +0900

    Algorithm Working

--- NIST.py
+++ NIST.py
@@ -21,6 +21,7 @@ import statistics as st
 from array import array
 from skimage.filters.rank import entropy as en
 from skimage.morphology import disk
+#import imutiimls 
 import os
 
 
@@ -41,66 +42,126 @@ def get_image_paths(imagename):
     """
     abs_path = os.path.abspath(__file__)
     dirname = os.path.dirname(abs_path)
-    print("Full path: " + abs_path)
-    print("Directory Path: " +  dirname)
+    #print("Full path: " + abs_path)
+    #print("Directory Path: " +  dirname)
     fullpath = os.path.join(dirname, imagename)
-    print("Full path: " + fullpath)
+    #print("Full path: " + fullpath)
     return fullpath
 
 
-
-
-def run_beta():
-    print("Beta Program is Started........... !!!")
+def getImageMark(originalImagePath="images/Bilal", dn=1):
     # Write code Here
-    dn = 1
-    imageReadPath = get_image_paths("images/Bilal.png")
+    imageReadPath = get_image_paths(originalImagePath+""+".png")
     image = Image.open(imageReadPath)
-    image.show()
-    imageGray = image.convert('L')
-    #imageGray.show()
-    imageGraySavePath = get_image_paths("images/BilalGray.png")
-    imageGray.save(imageGraySavePath)
-    imageGrayResize = imageGray.resize((int(imageGray.width / dn), int(imageGray.height / dn)))
-    #imageGrayResize.show()
-    imageGrayResizeSavePath = get_image_paths("images/BilalGrayResize.png")
-    imageGrayResize.save(imageGrayResizeSavePath)
-    image_array_resize = np.array(imageGrayResize)
     image_array = np.array(image)
+    imageGray = image.convert('L')
+    imageGrayResize = imageGray.resize((int(imageGray.width / dn), int(imageGray.height / dn)))
+    image_array_resize = np.array(imageGrayResize)
     imageAlpha = image.convert("RGBA") # sets alpha to 255
     image_array_Mark = np.array(imageAlpha)
-    #image_array_Mark = np.zeros((int(imageGray.height / dn), int(imageGray.width / dn),3)) #numpy.zeros((800, 800))
-    print("Image Marked   : ", image_array_Mark.shape)
-    print("Image Basic    : ", image_array.shape)
-    print("Image Sized    : ", image_array_resize.shape)
-    print("Image Length   : ", len(image_array))
-    print("Image Shape[0] : ", image_array.shape[0])
-    print("Image Shape[1] : ", image_array.shape[1])
-    print("Image Shape[2] : ", image_array.shape[2])
-
-    
+    #print("Image Shape : ", image_array_Mark.shape)
+    a = image_array.shape[2]
     for i in range(image_array.shape[0]):
          for j in range(image_array.shape[1]):
-            image_array_Mark[i][j][image_array.shape[2]] = image_array_resize[i][j]
-            #for k in range(image_array.shape[2]):
-             #   image_array_Mark[i][j][k] = image_array[i][j][k]
-            #print("image_array[",i,"][",j,"][",[image_array.shape[2]-1],"]",image_array[i][j][image_array.shape[2]-1]) 
-    # for i in range(image_array_resize.shape[0]):
-    #      for j in range(image_array_resize.shape[1]):
-    #         image_array_Mark[i][j][4] = image_array_resize[i][j]       
-    #         for k in range(image_array.shape[2]):
-    #             print("image_array[",i,"][",j,"][",k,"]",image_array[i][j][k]) 
+            image_array_Mark[i][j][a] = image_array_resize[i][j]
     imageMarked=Image.fromarray(image_array_Mark)
-    imageMarkSavePath = get_image_paths("images/BilalMarked.png")
+    imageMarkSavePath = get_image_paths(originalImagePath+"Marked"+".png")
     imageMarked.save(imageMarkSavePath)
-    #imageMarked.show()
-    imageMarkedReadPath = get_image_paths("images/BilalMarked.png")
-    imageMarkedLoaded = Image.open(imageMarkedReadPath)
-    imageMarkedLoaded.show()
-    imageMarkedLoadedArray = np.array(imageMarkedLoaded)
-    print("Image Marked Loaded   : ", imageMarkedLoadedArray.shape)
-    imageSavePath = get_image_paths("images/Bilal.png")
-    image.save(imageSavePath)
+    return imageMarked
+
+def isImageMark(originalImagePath="images/Bilal", dn=1):
+    # Write code Here
+    imageReadPath = get_image_paths(originalImagePath+""+".png")
+    imageMarkReadPath = get_image_paths(originalImagePath+""+".png")
+    image = Image.open(imageReadPath)
+    imageMark = Image.open(imageMarkReadPath)
+    image_array_Mark = np.array(imageMark)
+    image_array = np.array(image)
+    imageGray = image.convert('L')
+    imageGrayResize = imageGray.resize((int(imageGray.width / dn), int(imageGray.height / dn)))
+    image_array_resize = np.array(imageGrayResize)
+    matchNumber = 0
+    totalNumber = 0
+    try:
+        a = image_array.shape[2]
+        for i in range(image_array.shape[0]):
+            for j in range(image_array.shape[1]): 
+                if (image_array_Mark[i][j][a-1] == image_array_resize[i][j]):
+                    matchNumber = matchNumber+1
+
+                totalNumber = totalNumber +1
+        if (matchNumber==totalNumber):
+            print("Image is Marked Properly!!!")
+            print("Marking Ratio : ", int(matchNumber/totalNumber))
+            return True
+        else: 
+            print("Image is Not Marked Properly !!!")
+            print("Marking Ratio : ", int(matchNumber/totalNumber))
+            return False
+    except:
+        print("Scalar Image Error: Image is Not Marked Properly !!!")
+        print("Marking Ratio : ", int(0))
+        return False
+
+
+
+def run_beta(originalImagePath="images/Bilal", markImagePath="images/BilalMarked.png"):
+    print("Beta Program is Started........... !!!")
+    # Write code Here
+    imageName = "images/Bilal"
+    imageNameMark = "images/BilalGray"
+    getImageMark(imageName).show()
+    isImageMark(imageNameMark)
+
+
+    # dn = 1
+    # imageReadPath = get_image_paths("images/Bilal.png")
+    # image = Image.open(imageReadPath)
+    # image.show()
+    # imageGray = image.convert('L')
+    # #imageGray.show()
+    # imageGraySavePath = get_image_paths("images/BilalGray.png")
+    # imageGray.save(imageGraySavePath)
+    # imageGrayResize = imageGray.resize((int(imageGray.width / dn), int(imageGray.height / dn)))
+    # #imageGrayResize.show()
+    # imageGrayResizeSavePath = get_image_paths("images/BilalGrayResize.png")
+    # imageGrayResize.save(imageGrayResizeSavePath)
+    # image_array_resize = np.array(imageGrayResize)
+    # image_array = np.array(image)
+    # imageAlpha = image.convert("RGBA") # sets alpha to 255
+    # image_array_Mark = np.array(imageAlpha)
+    # #image_array_Mark = np.zeros((int(imageGray.height / dn), int(imageGray.width / dn),3)) #numpy.zeros((800, 800))
+    # print("Image Marked   : ", image_array_Mark.shape)
+    # print("Image Basic    : ", image_array.shape)
+    # print("Image Sized    : ", image_array_resize.shape)
+    # print("Image Length   : ", len(image_array))
+    # print("Image Shape[0] : ", image_array.shape[0])
+    # print("Image Shape[1] : ", image_array.shape[1])
+    # print("Image Shape[2] : ", image_array.shape[2])
+
+    
+    # for i in range(image_array.shape[0]):
+    #      for j in range(image_array.shape[1]):
+    #         image_array_Mark[i][j][image_array.shape[2]] = image_array_resize[i][j]
+    #         #for k in range(image_array.shape[2]):
+    #          #   image_array_Mark[i][j][k] = image_array[i][j][k]
+    #         #print("image_array[",i,"][",j,"][",[image_array.shape[2]-1],"]",image_array[i][j][image_array.shape[2]-1]) 
+    # # for i in range(image_array_resize.shape[0]):
+    # #      for j in range(image_array_resize.shape[1]):
+    # #         image_array_Mark[i][j][4] = image_array_resize[i][j]       
+    # #         for k in range(image_array.shape[2]):
+    # #             print("image_array[",i,"][",j,"][",k,"]",image_array[i][j][k]) 
+    # imageMarked=Image.fromarray(image_array_Mark)
+    # imageMarkSavePath = get_image_paths("images/BilalMarked.png")
+    # imageMarked.save(imageMarkSavePath)
+    # #imageMarked.show()
+    # imageMarkedReadPath = get_image_paths("images/BilalMarked.png")
+    # imageMarkedLoaded = Image.open(imageMarkedReadPath)
+    # imageMarkedLoaded.show()
+    # imageMarkedLoadedArray = np.array(imageMarkedLoaded)
+    # print("Image Marked Loaded   : ", imageMarkedLoadedArray.shape)
+    # imageSavePath = get_image_paths("images/Bilal.png")
+    # image.save(imageSavePath)
     print("Beta Program is Ended Successfully !!!")
     return BETA
 
